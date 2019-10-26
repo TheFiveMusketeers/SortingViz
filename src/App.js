b058fc2031d2776137e3fcaf824fb13829827309
Justin Ong <cjto2000@gmail.com>
2019-10-25 23:07:34 -0700

    basic graph

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import Visual from "./components/sorting_chart"
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Visual></Visual>
+    </div>
+  );
+}
+
+export default App;
